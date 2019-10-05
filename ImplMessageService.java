3c73bf5178a0bdadb06df0cc29d6098104e7fee7
Kid1995 <kietdt2307@gmail.com>
2019-10-05 21:21:53 +0200

    Revert " use UnicastRemoteObject statt Registry Class"
    This reverts commit be36da048b6e184cf7cbb702c316931e04a36102.

--- ImplMessageService.java
+++ ImplMessageService.java
@@ -1,19 +1,15 @@
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 
 // Servant Class für Dispatcher Skeleton und alle davon unterstürzen entfernten Objekt
 
 //TODO: ADD RECEIVED TIMESTAMP TO "newMessage()" Function
 
-public class ImplMessageService extends UnicastRemoteObject implements MessageService {
+public class ImplMessageService implements MessageService {
 
+    private static int MAX_MSG_CAPACITY = 10;
     private int msgID = 0;
-    private DeliveryQueue messageQueue;
 
-    protected ImplMessageService( int msgCapacity) throws RemoteException {
-        super();
-        this.messageQueue = new DeliveryQueue(msgCapacity);
-    }
+    private DeliveryQueue messageQueue = new DeliveryQueue(MAX_MSG_CAPACITY);
 
 
     @Override
--- Server.java
+++ Server.java
@@ -1,17 +1,16 @@
-import java.rmi.Naming;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Server  {
+public class Server extends ImplMessageService {
 
     public Server() {}
 
     public static void main(String args[]) {
         try {
-            /*Instantiating the implementation class
-            ImplMessageService obj = new ImplMessageService(20);
+            // Instantiating the implementation class
+            ImplMessageService obj = new ImplMessageService();
 
             // Exporting the object of implementation class
             // (here we are exporting the remote object to the stub)
@@ -21,9 +20,6 @@ public class Server  {
             Registry registry = LocateRegistry.getRegistry();
 
             registry.bind("MessageService", stub);
-             */
-
-            Naming.rebind("//localhost/MessageService", new ImplMessageService(20));
             System.err.println("Server ready");
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
