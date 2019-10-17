0d025a665d995af0775d99e49b243c886f5dc776
Wüsthoff <Sofian.Wuesthoff@haw-hamburg.de>
2019-10-17 17:08:28 +0200

    Gui angefangen zu ändern

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -11,19 +12,24 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Client extends Application {
-    private static final String QUIT = "Q";
-    private static final String GET = "G";
-    private static final String SEND = "S";
+	private static final String QUIT = "Quit";
+	private static final String GET = "Get";
+	private static final String SEND = "Send";
 	private static final int SERVICE_PORT = 1099;
 	private static final String SERVICE_NAME = "MessageService";
 	// private static final String SERVER_IP = "192.168.178.20";
@@ -61,21 +67,14 @@ public class Client extends Application {
 		}
 
 		Text text = new Text("");
-		text.setX(50);
-		text.setY(50);
 		text.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+		text.setText("Please enter your ID");
 
 		Text infoText = new Text("");
 		infoText.setX(50);
 		infoText.setY(260);
 		infoText.setFill(Color.GREEN);
 		infoText.setFont(Font.font("Verdana", 15));
-
-		Text cmdInfo = new Text("Command Info: \n \n" + "Q: Quits out of the application \n \n"
-				+ "G: Gets a message from the Delivery Queue \n \n" + "S: Sends a message to the Delivery Queue");
-		cmdInfo.setX(350);
-		cmdInfo.setY(110);
-		cmdInfo.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
 
 		Text clientIDText = new Text("");
 		clientIDText.setX(550);
@@ -87,33 +86,33 @@ public class Client extends Application {
 		msgBoxText.setY(170);
 		msgBoxText.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
 
-		// Creating a text filed
-		TextField textField = new TextField();
-
-		// Setting the position of the text field
-		textField.setLayoutX(50);
-		textField.setLayoutY(100);
-
 		TextField msgField = new TextField();
-		msgField.setLayoutX(50);
-		msgField.setLayoutY(180);
 
-		textField.setOnAction(new EventHandler<ActionEvent>() {
+		ComboBox<String> comboBox = new ComboBox<String>();
+		comboBox.getItems().addAll("Quit", "Send", "Get");
+
+		TextArea textArea = new TextArea("");
+
+		msgField.setOnAction(new EventHandler<ActionEvent>() {
 			String msg = "";
 			String clientID = "";
 
 			@Override
 			public void handle(ActionEvent event) {
-				text.setText("Please enter your command");
-				if (event.getEventType().equals(ActionEvent.ACTION)) {
-					if (clientID.equals("")) {
-                        clientID = textField.getText();
-                        clientIDText.setText("ClientID: " + clientID);
-                        textField.clear();
-                    } else {
-						switch (textField.getText()) {
+				if (clientID.equals("")) {
+					clientID = msgField.getText();
+					clientIDText.setText(clientID);
+					msgField.clear();
+				}
+				comboBox.setOnAction(new EventHandler<ActionEvent>() {
+
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Test1");
+
+						switch (comboBox.getValue()) {
 						case QUIT:
-							textField.clear();
+							System.out.println("QUIT");
 							try {
 								Platform.exit();
 								System.exit(0);
@@ -123,17 +122,13 @@ public class Client extends Application {
 							}
 							break;
 						case GET:
-							textField.clear();
-							for (int i = 0; i >= 0; i--) {
-								try {
-									msg = stub.nextMessage(clientID);
-								} catch (RemoteException e) {
-									infoText.setText("Message could not be got!");
-									remoteMethodeHandle();
-									i++;
-									System.out.println("Get Exception");
-									// e.printStackTrace();
-								}
+							try {
+								msg = stub.nextMessage(clientID);
+							} catch (RemoteException e) {
+								infoText.setText("Message could not be recevied!");
+								System.out.println("Get Exception");
+								Platform.exit();
+								System.exit(0);
 							}
 							if (msg != null) {
 
@@ -144,7 +139,6 @@ public class Client extends Application {
 							text.setText("Please enter your command!");
 							break;
 						case SEND:
-							textField.clear();
 							text.setText("Please enter your message in the textfield below!");
 							msgField.setOnAction(new EventHandler<ActionEvent>() {
 								@Override
@@ -153,16 +147,17 @@ public class Client extends Application {
 										msg = msgField.getText();
 										System.out.println(msg);
 										for (int i = 0; i >= 0; i--) {
-											try {
-												stub.newMessage(clientID, msg);
-												msgField.clear();
-											} catch (RemoteException e) {
-												infoText.setText("Message could not be sent!");
-												remoteMethodeHandle();
-												i++;
-												System.out.println("Send Exception");
-												// e.printStackTrace();
-											}
+//											try {
+//												stub.newMessage(clientID, msg);
+//												msgField.clear();
+//											} catch (RemoteException e) {
+//												infoText.setText("Message could not be sent!");
+//												remoteMethodeHandle();
+//												i++;
+//												System.out.println("Send Exception");
+//												// e.printStackTrace();
+//											}
+											System.out.println("asd");
 										}
 										infoText.setText("Message sent!");
 										text.setText("Please enter your command!");
@@ -172,19 +167,42 @@ public class Client extends Application {
 							});
 							break;
 						}
-				}
-				}
+					}
+				});
 			}
 		});
 
-		// Creating a Group object
-		Group root = new Group(textField, text, msgField, infoText, cmdInfo, clientIDText, msgBoxText);
-
 		// Creating a scene object
-		Scene scene = new Scene(root, 800, 300);
-		stage.setTitle(ip);
+
+		// GridPane grid = new GridPane();
+		// grid.add(comboBox,30,0);
+		// grid.add(new Label("Commands:"), 2, 0);
+		//
+		// // Creating a Group object
+		// Group root = new Group();
+		// root.getChildren().add(grid);
+		// Scene scene = new Scene(root, 800, 300);
+		// stage.setTitle(ip);
+		// stage.setScene(scene);
+		// stage.show();
+
+		Scene scene = new Scene(new Group(), 450, 250);
+		GridPane grid = new GridPane();
+		grid.setVgap(4);
+		grid.setHgap(10);
+		grid.setPadding(new Insets(5, 5, 5, 5));
+		grid.add(new Label("Commands :"), 0, 0);
+		grid.add(comboBox, 1, 0);
+		grid.add(new Label("ClientID:"), 2, 0);
+		grid.add(clientIDText, 3, 0);
+		grid.add(new Label("Messages: "), 0, 1);
+		grid.add(msgField, 1, 1, 3, 1);
+
+		Group root = (Group) scene.getRoot();
+		root.getChildren().add(grid);
 		stage.setScene(scene);
 		stage.show();
+
 	}
 
 	private void remoteMethodeHandle() {
